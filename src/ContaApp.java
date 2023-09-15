6eaaa4055e3d3a2e6b1b7fc0fab5bd638e7ad8f4
Gustavo Sganderla <110643171+Gsgandela@users.noreply.github.com>
2023-09-14 21:17:42 -0300

    Trabalho 02

--- src/ContaApp.java
+++ src/ContaApp.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class Conta {
+public class ContaApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculadora calculadora = new Calculadora();
